e7ab70d53c3aecdef1f43dd2d8316bb2dbeb1a8a
Enes Telli <55920002+enes-telli@users.noreply.github.com>
2020-09-10 13:29:39 +0300

    Create Day0:HelloWorld.java

---
+++ 30 Days of Code/Day 0: Hello, World./Day0:HelloWorld.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+        System.out.println("Hello, World.\n" + inputString);
+	}
+}
