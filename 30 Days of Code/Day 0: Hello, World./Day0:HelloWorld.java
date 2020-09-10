4d42d590d7bb1cfecdae889d2801877c0fa5474b
Enes Telli <55920002+enes-telli@users.noreply.github.com>
2020-09-10 13:56:28 +0300

    Update Day0:HelloWorld.java

--- 30 Days of Code/Day 0: Hello, World./Day0:HelloWorld.java
+++ 30 Days of Code/Day 0: Hello, World./Day0:HelloWorld.java
@@ -6,8 +6,8 @@ import java.util.regex.*;
 
 public class Solution {
     public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    String inputString = scan.nextLine();
-    System.out.println("Hello, World.\n" + inputString);
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+        System.out.println("Hello, World.\n" + inputString);
     }
 }
