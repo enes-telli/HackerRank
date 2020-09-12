eb1f35290593eb133946e578632db19f2d35d0dc
Enes Telli <55920002+enes-telli@users.noreply.github.com>
2020-09-12 18:07:25 +0300

    Update solution.java

--- 30 Days of Code/Day 2: Operators/solution.java
+++ 30 Days of Code/Day 2: Operators/solution.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Solution {
     static void solve(double meal_cost, int tip_percent, int tax_percent) {
-        System.out.println(Math.round(meal_cost + meal_cost * (tip_percent + tax_percent) / 100 ));
+        System.out.println(Math.round(meal_cost + meal_cost * (tip_percent + tax_percent) / 100));
     }
 
     public static void main(String[] args) {
