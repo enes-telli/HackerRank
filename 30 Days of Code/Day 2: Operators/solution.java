7175127dbfb0e6973b1e3d95cfe805d61294ccd4
Enes Telli <55920002+enes-telli@users.noreply.github.com>
2020-09-12 18:01:21 +0300

    Update and rename Day2:Operators.java to solution.java

---
+++ 30 Days of Code/Day 2: Operators/solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        System.out.println(Math.round(meal_cost + meal_cost * (tip_percent + tax_percent) / 100 ));
+    }
+
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double meal_cost = scanner.nextDouble();
+        int tip_percent = scanner.nextInt();
+        int tax_percent = scanner.nextInt();
+        solve(meal_cost, tip_percent, tax_percent);
+    }
+}
