94216a588e48944d5fed5fdf3bb33fd3f3eec471
Enes Telli <55920002+enes-telli@users.noreply.github.com>
2020-09-15 19:23:53 +0300

    Create solution.java

---
+++ 30 Days of Code/Day _14: Scope/solution.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+    
+    public Difference(int[] elements) {
+        this.elements = elements;
+    }
+
+    public void computeDifference() {
+        int max = elements[0];
+        int min = elements[0];
+        for(int i = 1; i < this.elements.length; i++) {
+            if (elements[i] > max)
+                max = elements[i];
+            else if (elements[i] < min)
+                min = elements[i];
+        }
+        maximumDifference = max - min;
+    }
+
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
