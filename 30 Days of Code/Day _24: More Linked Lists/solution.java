f82bc723437551148b5721db88d0011a45f19940
Enes Telli <55920002+enes-telli@users.noreply.github.com>
2020-09-25 15:29:50 +0300

    Update solution.java

--- 30 Days of Code/Day _24: More Linked Lists/solution.java
+++ 30 Days of Code/Day _24: More Linked Lists/solution.java
@@ -23,8 +23,8 @@ class Solution
         return head;
     }
 
-	 public static  Node insert(Node head,int data)
-     {
+    public static  Node insert(Node head,int data)
+    {
         Node p=new Node(data);			
         if(head==null)
             head=p;
@@ -41,25 +41,25 @@ class Solution
         return head;
     }
     public static void display(Node head)
+    {
+        Node start=head;
+        while(start!=null)
         {
-              Node start=head;
-              while(start!=null)
-              {
-                  System.out.print(start.data+" ");
-                  start=start.next;
-              }
+            System.out.print(start.data+" ");
+            start=start.next;
         }
-        public static void main(String args[])
-        {
-              Scanner sc=new Scanner(System.in);
-              Node head=null;
-              int T=sc.nextInt();
-              while(T-->0){
-                  int ele=sc.nextInt();
-                  head=insert(head,ele);
-              }
-              head=removeDuplicates(head);
-              display(head);
-
-       }
     }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        Node head=null;
+        int T=sc.nextInt();
+        while(T-->0){
+            int ele=sc.nextInt();
+            head=insert(head,ele);
+        }
+        head=removeDuplicates(head);
+        display(head);
+
+     }
+}
